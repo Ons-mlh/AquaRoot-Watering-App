66664d98101e361a303781963c3235ce827bd46b
Ons <ons.el.maleh@gmail.com>
2025-03-31 13:19:37 +0100

    feat: improved UI and added floating animation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ export default function Contact() {
     return (
         <>
             <div className="flex bottom-0 w-full fixed justify-center items-end my-2 h-fit-content">
-                <h3 className="text-s font-bold text-[#114C82]">Made by Ons El Maleh</h3>
+                <h3 className="text-s font-bold text-[#114C82]">Made with 💙 by Ons El Maleh</h3>
                 <ul className='flex space-x-4 space-y-1 items-center justify-around ml-2'>
                     <li><a href="https://www.linkedin.com/in/ons-el-maleh-648531300/"><img src={linkedin} alt="linkedin" className='size-5 mt-1'/></a></li>
                     <li><a href="https://github.com/Ons-mlh"><img src={github} alt="github" className='size-5'/></a></li>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,22 @@
 import { useState } from 'react';
 import plant from '../assets/plant.png';
+import cactusIcon from '../assets/icons/cactus.png';
+import flowerIcon from '../assets/icons/flower.png';
 
 export default function Home() {
     return (
-        <>
-            <div className='flex flex-col items-center mt-20'>
-                <div className='bg-[#BECDAB] w-80 h-80 rounded-[50%] shadow-emerald-200'>
-                    <img src={plant} alt="" />
-                </div>
-                <p className='text-center text-green-700 text-3xl font-bold mt-10'>
-                    Plants give us life, Let’s return the favor! <br></br>Welcome to AquaRoot, your partner in mindful plant care
-                </p>
+        <> 
+                <div className='relative w-fit-content'>
+                    <img src={cactusIcon} alt="cactus icon" className="size-15 absolute animate-bounce left-350 top-90 opacity-40 "/>
+                    <img src={flowerIcon} alt="flower icon" className="size-15 absolute animate-bounce left-50 top-15 opacity-40 "/>
+                    <div className='flex flex-col items-center mt-20'>
+                        <div className='bg-[#BECDAB] w-80 h-80 rounded-[50%] shadow-emerald-200'>
+                            <img src={plant} alt="plant" />
+                        </div>
+                        <p className='text-center text-green-700 text-3xl font-bold mt-13'>
+                            Plants give us life, Let’s return the favor! <br></br>Welcome to AquaRoot, your partner in mindful plant care
+                        </p>
+                    </div>
             </div>
         </>
     )
